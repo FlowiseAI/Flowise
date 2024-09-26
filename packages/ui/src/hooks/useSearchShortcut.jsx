FlowiseAI/Flowise

commit 4381656a6e579e57b709015a01c887df7fe86898

feat: add search keyboard shortcut based on the current platform (#3267)

packages/ui/src/hooks/useSearchShortcut.jsx
@@ -1,0 +1,30 @@
+import { useEffect } from 'react'
+import { getOS } from '@/utils/genericHelper'
+
+const isMac = getOS() === 'macos'
+
+const useSearchShorcut = (inputRef) => {
+    useEffect(() => {
+        const component = inputRef.current
+        const handleKeyDown = (event) => {
+            if ((isMac && event.metaKey && event.key === 'f') || (!isMac && event.ctrlKey && event.key === 'f')) {
+                event.preventDefault()
+                component.focus()
+            }
+        }
+
+        const handleInputEscape = (event) => {
+            if (event.key === 'Escape') component.blur()
+        }
+
+        inputRef.current.addEventListener('keydown', handleInputEscape)
+        document.addEventListener('keydown', handleKeyDown)
+
+        return () => {
+            component.addEventListener('keydown', handleInputEscape)
+            document.removeEventListener('keydown', handleKeyDown)
+        }
+    })
+}
+
+export default useSearchShorcut

packages/ui/src/layout/MainLayout/ViewHeader.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types'
+import { useRef } from 'react'
 
 // material-ui
 import { IconButton, Box, OutlinedInput, Toolbar, Typography } from '@mui/material'
@@ -7,6 +8,8 @@ import { StyledFab } from '@/ui-component/button/StyledFab'
 
 // icons
 import { IconSearch, IconArrowLeft, IconEdit } from '@tabler/icons-react'
+
+import useSearchShorcut from '@/hooks/useSearchShortcut'
 
 const ViewHeader = ({
     children,
@@ -22,6 +25,8 @@ const ViewHeader = ({
     onEdit
 }) => {
     const theme = useTheme()
+    const searchInputRef = useRef()
+    useSearchShorcut(searchInputRef)
 
     return (
         <Box sx={{ flexGrow: 1, py: 1.25, width: '100%' }}>
@@ -85,6 +90,7 @@ const ViewHeader = ({
                 <Box sx={{ height: 40, display: 'flex', alignItems: 'center', gap: 1 }}>
                     {search && (
                         <OutlinedInput
+                            inputRef={searchInputRef}
                             size='small'
                             sx={{
                                 width: '280px',

commit 4a1f5bf1e924cf6e66fbe468111572c048356b00

fix: addEventListener error on views without search box introduced by commit #3267
The error was caused when trying to add an event listener to an undefined search box in views without one like tools and assistants

packages/ui/src/hooks/useSearchShortcut.jsx
@@ -5,6 +5,7 @@ const isMac = getOS() === 'macos'
 
 const useSearchShorcut = (inputRef) => {
     useEffect(() => {
+        if (!inputRef.current) return
         const component = inputRef.current
         const handleKeyDown = (event) => {
             if ((isMac && event.metaKey && event.key === 'f') || (!isMac && event.ctrlKey && event.key === 'f')) {